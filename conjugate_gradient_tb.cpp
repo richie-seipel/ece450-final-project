2024907fabcc5e4e43908eac9379282aee91edd0
rts325 <rts325@lehigh.edu>
2025-11-24 16:29:24 -0500

    verified code works need to generate matrices

--- conjugate_gradient_tb.cpp
+++ conjugate_gradient_tb.cpp
@@ -4,14 +4,25 @@
 
 int main(){
     float matrix1[N][N] = {{2, 1, 0, 0}, {1, 2, 1, 0}, {0, 1, 2, 1}, {0, 0, 1, 2}};
-    float matrix2[N] = {1, 2, 3, 4};
+    float matrix2[N] = {3, 2, 6, 4};
     //initial guess
-    float result[N] = {0, 0, 0, 0};
+    float result[N] = {1, 1, 1, 1};
 
     conjugate_gradient(matrix1, matrix2, result, 0.001);
     std::cout << "conjugate gradient" << std::endl;
-    for (const auto& element : result) {
-        std::cout << element << " ";
+    for (int i = 0; i < N; i ++) {
+        for(int j = 0; j < N; j ++){
+            std::cout << matrix1[i][j] << " ";
+        }
+        std::cout << result[i] << " " << matrix2[i] << std::endl;
+    }
+
+    for (int i = 0; i < N; i ++) {
+        float sum = 0;
+        for(int j = 0; j < N; j ++){
+            sum += matrix1[i][j] * result[j];
+        }
+        std::cout << (std::round(sum) == matrix2[i]) << std::endl;
     }
 
     return 0;
