967d3d93b37c7de9376f436c078c33148896fdb3
rts325 <rts325@lehigh.edu>
2025-11-24 15:32:32 -0500

    new implementation

---
+++ new_opt_conjugate_gradient.cpp
@@ -0,0 +1,62 @@
+#include <hls_stream.h>
+#include <math.h>
+#include <stdio.h>
+
+#define N 4               // Matrix dimension (4x4 )
+//#define MAX_ITER 4//shoulbe equal to N
+typedef float data_t;
+
+void cg(data_t A[N][N], data_t b[N], data_t x[N], data_t tol) {
+#pragma HLS ARRAY_PARTITION variable=A complete dim=2
+#pragma HLS ARRAY_PARTITION variable=A complete dim=1
+#pragma HLS PIPELINE
+
+    data_t r[N], p[N], Ap[N];
+    data_t rsold, rsnew, alpha, beta, pAp;
+
+    // Initial residual r = b - A*x
+    for (int i = 0; i < N; i++) {
+        data_t sum = 0;
+        for (int j = 0; j < N; j++) sum += A[i][j] * x[j];
+        r[i] = b[i] - sum;
+        p[i] = r[i];
+    }
+
+    // rsold = r^T r
+    rsold = 0;
+    for (int i = 0; i < N; i++) rsold += r[i] * r[i];
+
+    cg_loop:
+    for (int k = 0; k < N; k++) {
+    #pragma HLS PIPELINE
+        // Ap = A * p
+        for (int i = 0; i < N; i++) {
+            data_t sum = 0;
+            for (int j = 0; j < N; j++)
+                sum += A[i][j] * p[j];
+            Ap[i] = sum;
+        }
+
+        // pAp = p^T * Ap
+        pAp = 0;
+        for (int i = 0; i < N; i++) pAp += p[i] * Ap[i];
+
+        alpha = rsold / pAp;
+
+        // x = x + alpha * p
+        // r = r - alpha * Ap
+        for (int i = 0; i < N; i++) {
+            x[i] = x[i] + alpha * p[i];
+            r[i] = r[i] - alpha * Ap[i];
+        }
+
+        rsnew = 0;
+        for (int i = 0; i < N; i++) rsnew += r[i] * r[i];
+
+        if (sqrtf(rsnew) < tol) break;
+
+        beta = rsnew / rsold;
+        for (int i = 0; i < N; i++) p[i] = r[i] + beta * p[i];
+        rsold = rsnew;
+    }
+}
