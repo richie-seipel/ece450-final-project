12f6d9173bf80fa16c34555f040ecb7d5d05f107
rts325 <rts325@lehigh.edu>
2025-11-25 12:32:36 -0500

    method to fill matrix in tb

--- conjugate_gradient_tb.cpp
+++ conjugate_gradient_tb.cpp
@@ -1,10 +1,32 @@
 #include "new_conjugate_gradient.h"
 
+void generate_spd_matrix(data_t A[N][N]) {
+    //fill matrix
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            A[i][j] = 1 + (i+1)*(j+1);
+        }
+    }
+
+    //make symmetric
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++){
+            A[j][i] = A[i][j];
+        }
+    }
+
+}
+
 int main(){
-    float matrix1[N][N] = {{2, 1, 0, 0}, {1, 2, 1, 0}, {0, 1, 2, 1}, {0, 0, 1, 2}};
-    float matrix2[N] = {3, 2, 6, 4};
+    float matrix1[N][N];
+    generate_spd_matrix(matrix1);
+    float matrix2[N];
     //initial guess
-    float result[N] = {1, 1, 1, 1};
+    float result[N];
+    for(int i = 0; i < N; i++){
+        matrix2[i] = i;
+        result[i] = 1;
+    }
 
     conjugate_gradient(matrix1, matrix2, result, 0.001);
     std::cout << "conjugate gradient" << std::endl;
@@ -20,7 +42,12 @@ int main(){
         for(int j = 0; j < N; j ++){
             sum += matrix1[i][j] * result[j];
         }
-        std::cout << (std::round(sum) == matrix2[i]) << std::endl;
+        if (std::round(sum) == matrix2[i]){
+            std::cout << "Value " << i << " correct" << std::endl;
+        }
+        else {
+            std::cout << "Value " << i << " incorrect" << std::endl;
+        }
     }
 
     return 0;
